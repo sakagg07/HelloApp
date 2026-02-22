e8a56b151ae9aa776f137935f037559165edebaa
Sakshi Agrawal <sakshi@Sakshis-MacBook-Air.local>
2026-02-22 23:51:21 +0530

    Initial commit - Java Banner App

---
+++ BannerApp.java
@@ -0,0 +1,10 @@
+public class BannerApp {
+    public static void main(String[] args) {
+
+        System.out.println("*************");
+        System.out.println("*   OOPS    *");
+        System.out.println("*  BANNER   *");
+        System.out.println("*************");
+
+    }
+}
