6622d8b135179018c0866b90f2a34c87e0f5378b
Sakshi Agrawal <sakshi@Sakshis-MacBook-Air.local>
2026-02-23 00:53:35 +0530

    Updated code

--- BannerApp.java
+++ BannerApp.java
@@ -1,8 +1,9 @@
 public class BannerApp {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println("***   ***  ****  ****");
+        System.out.println("*  * *  *  *   * *");
+        System.out.println("*  * *  *  ****   ***");
+        System.out.println("*  * *  *  *         *");
+        System.out.println("***   ***  *      ****");
     }
 }
-
-
-
