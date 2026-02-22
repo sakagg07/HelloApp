c03b58ed6722746b52da7864c2d863ac8001cb57
Sakshi Agrawal <sakshi@Sakshis-MacBook-Air.local>
2026-02-23 01:08:19 +0530

    Updated Java code

--- BannerApp.java
+++ BannerApp.java
@@ -1,21 +1,18 @@
 public class BannerApp {
-
-    public static String buildLine(String... parts) {
-        return String.join("", parts);
-    }
-
     public static void main(String[] args) {
 
-        String line1 = buildLine("***   ", "***  ", "****  ", "****");
-        String line2 = buildLine("*  * ", "*  *  ", "*   * ", "*");
-        String line3 = buildLine("*  * ", "*  *  ", "****   ", "***");
-        String line4 = buildLine("*  * ", "*  *  ", "*         ", "*");
-        String line5 = buildLine("***   ", "***  ", "*      ", "****");
+        // Storing banner lines in a String array (UC4 requirement)
+        String[] banner = {
+            " ***    ***    ****    **** ",
+            "*   *  *   *  *   *  *      ",
+            "*   *  *   *  ****    ***   ",
+            "*   *  *   *  *          *  ",
+            " ***    ***   *       ****  "
+        };
 
-        System.out.println(line1);
-        System.out.println(line2);
-        System.out.println(line3);
-        System.out.println(line4);
-        System.out.println(line5);
+        // Using enhanced for-loop to print banner (clean & reusable)
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
