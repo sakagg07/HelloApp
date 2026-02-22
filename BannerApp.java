c5c788b5e674c51b767927ace5f9968b6120c438
Sakshi Agrawal <sakshi@Sakshis-MacBook-Air.local>
2026-02-23 01:06:06 +0530

    Updated Java code

--- BannerApp.java
+++ BannerApp.java
@@ -1,5 +1,21 @@
-class Main {
+public class BannerApp {
+
+    public static String buildLine(String... parts) {
+        return String.join("", parts);
+    }
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        String line1 = buildLine("***   ", "***  ", "****  ", "****");
+        String line2 = buildLine("*  * ", "*  *  ", "*   * ", "*");
+        String line3 = buildLine("*  * ", "*  *  ", "****   ", "***");
+        String line4 = buildLine("*  * ", "*  *  ", "*         ", "*");
+        String line5 = buildLine("***   ", "***  ", "*      ", "****");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
     }
 }
